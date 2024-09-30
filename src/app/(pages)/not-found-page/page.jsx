5b6721ab36e467dc58d219e7f2a58aff15613369
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-09-30 14:14:24 +0300

    Add project folder structure

---
+++ src/app/(pages)/not-found-page/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Page({}) {
+  return (
+    <div><h1>Ooops! Page not found!</h1></div>
+  );
+};
---
+++ src/app/(pages)/tests/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function TestsPage({}) {
+  return (
+    <div>Tests page</div>
+  );
+};
